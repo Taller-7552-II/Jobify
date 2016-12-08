c98a9f451ffa5837cb4b3614bcddd974332f1eed
Gonzalo Guzzardi <gonzaloguzzardi@gmail.com>
2016-12-08 16:18:18 -0300

    tests

--- server/unit_tests/TopJobHandler_unittest.cpp
+++ server/unit_tests/TopJobHandler_unittest.cpp
@@ -36,6 +36,35 @@ TEST(TopJobHandlerTests, GetTopJobsGeneralOK){
 	delete conn;
 }
 
+TEST(TopJobHandlerTests, GetTopSkillOK){
+	Database* db = new DatabaseMockRAM;
+	TokenAuthenticator* tk = new TokenAuthenticator;
+	string token = tk->createToken("gonzalo");
+	TopJobHandler handler(db, tk);
+
+	vector<string> header_names;
+	header_names.push_back("conn_token");
+	vector<string> header_values;
+	header_values.push_back(token);
+	struct mg_connection* conn = new struct mg_connection;
+	struct http_message* hmsg = new_http_message("GET", "/skill/", "", &header_names, &header_values);
+
+	User user(db);
+	user.setUsername("gonzalo");
+	user.setPassword("qwerty");
+	Status s = user.DBcreate();
+	EXPECT_TRUE(s.ok());
+
+	HttpRequestConcrete req;
+	req.init(conn, hmsg);
+
+	handler.handle(req);
+	EXPECT_FALSE(req.getStatusCode() == HttpRequest::OK);
+
+	delete_http_message(hmsg);
+	delete conn;
+}
+
 TEST(TopJobHandlerTests, GetTopJobsOK){
 	Database* db = new DatabaseMockRAM;
 	TokenAuthenticator* tk = new TokenAuthenticator;
@@ -48,6 +77,35 @@ TEST(TopJobHandlerTests, GetTopJobsOK){
 	header_values.push_back(token);
 	struct mg_connection* conn = new struct mg_connection;
 	struct http_message* hmsg = new_http_message("GET", "/top_job/", "", &header_names, &header_values);
+
+	User user(db);
+	user.setUsername("gonzalo");
+	user.setPassword("qwerty");
+	Status s = user.DBcreate();
+	EXPECT_TRUE(s.ok());
+
+	HttpRequestConcrete req;
+	req.init(conn, hmsg);
+
+	handler.handle(req);
+	EXPECT_FALSE(req.getStatusCode() == HttpRequest::OK);
+
+	delete_http_message(hmsg);
+	delete conn;
+}
+
+TEST(TopJobHandlerTests, GetTopSkillsOK){
+	Database* db = new DatabaseMockRAM;
+	TokenAuthenticator* tk = new TokenAuthenticator;
+	string token = tk->createToken("gonzalo");
+	TopJobHandler handler(db, tk);
+
+	vector<string> header_names;
+	header_names.push_back("conn_token");
+	vector<string> header_values;
+	header_values.push_back(token);
+	struct mg_connection* conn = new struct mg_connection;
+	struct http_message* hmsg = new_http_message("GET", "/top_skill/", "", &header_names, &header_values);
 
 	User user(db);
 	user.setUsername("gonzalo");
