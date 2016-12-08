97566d15efce65504c9d3ded178256b9fa4b59a6
Gonzalo Guzzardi <gonzaloguzzardi@gmail.com>
2016-12-08 15:55:04 -0300

    tests

--- server/unit_tests/SkillGetHandler_unittest.cpp
+++ server/unit_tests/SkillGetHandler_unittest.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-TEST(SkillGetHandlerTests, UserGetSkillsGeneralOK){
+TEST(SkillGetHandlerTests, UserGetSkillsCategoriaOK){
 	Database* db = new DatabaseMockRAM;
 	TokenAuthenticator* tk = new TokenAuthenticator;
 	string token = tk->createToken("gonzalo");
@@ -18,7 +18,7 @@ TEST(SkillGetHandlerTests, UserGetSkillsGeneralOK){
 	vector<string> header_values;
 	header_values.push_back(token);
 	struct mg_connection* conn = new struct mg_connection;
-	struct http_message* hmsg = new_http_message("GET", "/skill/software/", "", &header_names, &header_values);
+	struct http_message* hmsg = new_http_message("GET", "/skill/software", "", &header_names, &header_values);
 
 	User user(db);
 	user.setUsername("gonzalo");
@@ -36,3 +36,31 @@ TEST(SkillGetHandlerTests, UserGetSkillsGeneralOK){
 	delete conn;
 }
 
+TEST(SkillGetHandlerTests, UserGetSkillsOK){
+	Database* db = new DatabaseMockRAM;
+	TokenAuthenticator* tk = new TokenAuthenticator;
+	string token = tk->createToken("gonzalo");
+	SkillGetHandler handler(db, tk);
+
+	vector<string> header_names;
+	header_names.push_back("conn_token");
+	vector<string> header_values;
+	header_values.push_back(token);
+	struct mg_connection* conn = new struct mg_connection;
+	struct http_message* hmsg = new_http_message("GET", "/skill/", "", &header_names, &header_values);
+
+	User user(db);
+	user.setUsername("gonzalo");
+	user.setPassword("qwerty");
+	Status s = user.DBcreate();
+	EXPECT_TRUE(s.ok());
+
+	HttpRequestConcrete req;
+	req.init(conn, hmsg);
+
+	handler.handle(req);
+	EXPECT_FALSE(req.getStatusCode() == HttpRequest::OK);
+
+	delete_http_message(hmsg);
+	delete conn;
+}
